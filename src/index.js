a83d98078f0fb1df2a238cb5bd1c6f91a2cbf582
Michael Strong <michael@fyresite.com>
2019-11-12 16:44:22 -0700

    eslint fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const Connection = require('./connection');
 
 function createConnection(options) {
+  // Hello
   return new Connection(options);
 }
 
 module.exports = {
   createConnection,
 };
-
-
